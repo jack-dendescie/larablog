bbb90a5a7beadc1a6afaf5abb6318d6bf03b0d16
Jacques AGBETI <Jacques.AGBETI@chem.loc>
2025-06-13 18:56:48 +0100

    Notifications- afficher les articles et les details d'un user donné

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PublicController;
+
 
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +28,8 @@ Route::middleware('auth')->group(function () {
     // Route::delete('/articles/{article}/remove', [UserController::class, 'remove'])->name('articles.remove');
     Route::delete('/articles/{id}', [UserController::class, 'remove'])->name('articles.remove');
 
+    Route::get('/{user}', [PublicController::class, 'index'])->name('public.index');
+    Route::get('/{user}/{article}', [PublicController::class, 'show'])->name('public.show');
 });
 
 
